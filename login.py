02543faaf06968fa5c08cc86a502b9ebc97775eb
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-12-20 09:46:41 +0300

    login simple code committed

--- login.py
+++ login.py
@@ -7,4 +7,9 @@ print('Login now !')
 username2 = input('Enter username :')
 password2 = input ('Enter password :')
 
+if username == username2 and password == password2 :
+    print('Logged in successfully !')
+else : 
+    print('Invalid Credential')
+
 
