d1a38d40e8558319beba637d18a501c79172994a
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-12-20 15:27:35 +0300

    changes made in counter counting amount and index html

--- myapp/myapplication/views.py
+++ myapp/myapplication/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index (request) :
-    return render (request, 'index.html')
+def index(request):
+    return render(request, 'index.html')
 
-def counter (request) : 
-    return render (request, 'counter.html')
+def counter(request):
+    if 'text' in request.GET:
+        text = request.GET['text']
+        amount_of_words = len(text.split())
+        return render(request, 'counter.html', {'amount': amount_of_words})
+    else:
+        pass 
