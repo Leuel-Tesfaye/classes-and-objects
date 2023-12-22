c713f4499619a18f818e84f03bdefc68870176cb
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-12-22 05:29:26 +0300

    changes made in index html features page

--- myapp/myapplication/views.py
+++ myapp/myapplication/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Features
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    Feature = Features()
+    Feature.id = 0
+    Feature.name = 'Fast'
+    Feature.details = 'Our services are faster than anyone else in the sector.'
+
+    Feature2 = Features()
+    Feature2.id = 1
+    Feature2.name = 'Reliable'
+    Feature2.details = 'Our services are more reliable than anyone else in the sector.'
+
+    Feature3 = Features()
+    Feature3.id = 2
+    Feature3.name = 'Easy to use'
+    Feature3.details = 'Our services are simpler than anyone else in the sector.'
+
+    Feature4 = Features()
+    Feature4.id = 3
+    Feature4.name = 'Affordable'
+    Feature4.details = 'Our services are more affordable than anyone else in the sector.'
+
+    return render(request, 'index.html', {'Feature': Feature, 'Feature2': Feature2, 'Feature3': Feature3, 'Feature4': Feature4})
+
 
 def counter(request):
     if 'text' in request.POST:
