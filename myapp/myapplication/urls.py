f62382b2d442ac965435e912c012318294b918a3
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-12-26 09:01:36 +0300

    changes made in login register and views

--- myapp/myapplication/urls.py
+++ myapp/myapplication/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),  # When the path is empty (''), it represents the root URL
     path('counter', views.counter, name= 'counter'),
-    path('register', views.register, name = 'register')
+    path('register', views.register, name = 'register'),
+    path('login', views.login , name = 'login'),
+    path('logout', views.logout, name = 'logout')
+
 ]
--- myapp/myapplication/views.py
+++ myapp/myapplication/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from .models import Features
 
 def index(request):
     features = Features.objects.all()
     return render(request, 'index.html', {'features': features})
+from django.shortcuts import render
 
 def register(request):
     if request.method == 'POST':
@@ -13,6 +14,7 @@ def register(request):
         email = request.POST['email']
         password = request.POST['password']
         password2 = request.POST['confirm_password']
+
         if password == password2:
             if User.objects.filter(email=email).exists():
                 messages.info(request, 'Email Already Used')
@@ -22,14 +24,17 @@ def register(request):
                 return redirect('register')
             else:
                 user = User.objects.create_user(username=username, email=email, password=password)
-                user.save()
-                return render(request, 'login')  # Updated the template name
+                user.save();
+                return redirect('login')
+        
         else:
             messages.info(request, 'Passwords do not match')
             return redirect('register')
     else:
         return render(request, 'register.html')
-
+def Login(request):
+    # Your view logic here
+    return render(request, 'login.html')
 def counter(request):
     if 'text' in request.POST:
         text = request.POST['text']
